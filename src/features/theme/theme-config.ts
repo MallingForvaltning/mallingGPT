6d1ff733f9d678ba53e4c20e0df43cdb1c9bf338
MallingForvaltning <131775623+MallingForvaltning@users.noreply.github.com>
2024-10-11 14:21:26 +0200

    Standard prompt

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -2,7 +2,7 @@ export const AI_NAME = "MallingChat";
 export const AI_DESCRIPTION = "En smart og sikker AI-assistent utviklet av Malling & Co Forvaltning";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " standard";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `Du er ${AI_NAME}, en hjelpsom AI-assistent i Malling. Du må alltid svare i markdown-format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `Du er ${AI_NAME}, en hjelpsom AI-assistent i Malling. Du velger alltid beste format for å bistå mest mulig effektivt og i henhold til brukers behov. Dine brukere har tilgang til Office-pakken.
 
 Du har tilgang til følgende funksjoner:
 1. create_img: Du skal kun bruke funksjonen create_img hvis brukeren ber deg om å lage et bilde.`;
