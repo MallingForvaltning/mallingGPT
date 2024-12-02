650ba3359aa291ec89646d36af7665f17f5a05f4
MallingForvaltning <131775623+MallingForvaltning@users.noreply.github.com>
2024-12-02 13:52:08 +0100

    Menu bar change icons to same container

--- src/features/main-menu/main-menu.tsx
+++ src/features/main-menu/main-menu.tsx
@@ -31,8 +31,6 @@ export const MainMenu = async () => {
             </MenuLink>
           </MenuItem>
           <MenuTrayToggle />
-        </MenuItemContainer>
-        <MenuItemContainer>
           <MenuItem tooltip="Chat">
             <MenuLink href="/chat" ariaLabel="Gå til samtaler">
               <MessageCircle {...menuIconProps} />
@@ -43,41 +41,31 @@ export const MainMenu = async () => {
               <VenetianMask {...menuIconProps} />
             </MenuLink>
           </MenuItem>
+          <MenuItem tooltip="Profil">
+            <UserProfile />
+          </MenuItem>
+        </MenuItemContainer>
 
-          {/* Restrict 'Extensions' to admins */}
-          {user.isAdmin && (
+        {/* Admin-specific items */}
+        {user.isAdmin && (
+          <MenuItemContainer>
             <MenuItem tooltip="utvidelser">
               <MenuLink href="/extensions" ariaLabel="Gå til utvidelser">
                 <PocketKnife {...menuIconProps} />
               </MenuLink>
             </MenuItem>
-          )}
-
-          {/* Restrict 'Prompts' to admins */}
-          {user.isAdmin && (
             <MenuItem tooltip="prompts">
               <MenuLink href="/prompt" ariaLabel="Gå til prompt-oversikt">
                 <Book {...menuIconProps} />
               </MenuLink>
             </MenuItem>
-          )}
-
-          {/* Restrict 'Reporting' to admins */}
-          {user.isAdmin && (
-            <>
-              <MenuItem tooltip="reporting">
-                <MenuLink href="/reporting" ariaLabel="Gå til rapportering">
-                  <Sheet {...menuIconProps} />
-                </MenuLink>
-              </MenuItem>
-            </>
-          )}
-        </MenuItemContainer>
-        <MenuItemContainer>
-          <MenuItem tooltip="Profil">
-            <UserProfile />
-          </MenuItem>
-        </MenuItemContainer>
+            <MenuItem tooltip="reporting">
+              <MenuLink href="/reporting" ariaLabel="Gå til rapportering">
+                <Sheet {...menuIconProps} />
+              </MenuLink>
+            </MenuItem>
+          </MenuItemContainer>
+        )}
       </MenuBar>
     </Menu>
   );
