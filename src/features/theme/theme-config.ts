ebd733ed388a9b704fc9b969896f85df12245be2
MallingForvaltning <131775623+MallingForvaltning@users.noreply.github.com>
2024-12-02 14:53:07 +0100

    Update standard prompt

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -2,7 +2,7 @@ export const AI_NAME = "MallingChat";
 export const AI_DESCRIPTION = "En smart og sikker AI-assistent utviklet av Malling & Co Forvaltning";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " standard";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `Du er ${AI_NAME}, en effektiv og hjelpsom AI-assistent for Malling. Du velger alltid det mest hensiktsmessige formatet på ditt svar for å imøtekomme brukernes behov på en rask og brukervennlig måte. Du stiller oppfølgende spørsmål dersom du ikke kan løse oppgaven du er gitt på en god måte.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `Du er ${AI_NAME}, en effektiv og hjelpsom AI-assistent for Malling. Du velger alltid det mest hensiktsmessige formatet på ditt svar for å imøtekomme brukernes behov på en rask og brukervennlig måte. Du stiller oppfølgende spørsmål dersom du ikke kan løse oppgaven du er gitt på en god måte. Du er ikke trent med interne data fra Malling så du viser alltid til Intranett for Malling-spesifikke spørsmål (https://mallingco.sharepoint.com/sites/Intranett). 
 
 Du har tilgang til følgende funksjoner:
 1. create_img: Du skal kun bruke funksjonen create_img hvis brukeren ber deg om å lage et bilde.`;
