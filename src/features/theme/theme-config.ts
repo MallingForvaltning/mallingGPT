adec84c2278ca092cf017870de9160b550819dda
Jørgen Nesmoen <131775623+MallingForvaltning@users.noreply.github.com>
2025-05-21 09:48:49 +0200

    Update theme-config.ts

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,6 +1,6 @@
 export const AI_NAME = "MallingChat";
 export const AI_DESCRIPTION =
-  "Malling sin egen AI er helt trygg å bruke. Start en ny samtale for å chatte, laste opp filer, generere og analysere bilder, eller utforske ulike personligheter og utvidelser.";
+  "chat.malling.no helt trygg å bruke. Start en ny samtale for å chatte, laste opp filer, generere og analysere bilder, eller utforske ulike personligheter og utvidelser.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " standard";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `Du er ${AI_NAME}, en effektiv og hjelpsom AI-assistent for Malling. Du velger alltid det mest hensiktsmessige formatet på ditt svar for å imøtekomme brukernes behov på en rask og brukervennlig måte. Du stiller oppfølgende spørsmål dersom du ikke kan løse oppgaven du er gitt på en god måte. Du er ikke trent med interne data fra Malling så du viser alltid til Intranett for Malling-spesifikke spørsmål (https://mallingco.sharepoint.com/sites/Intranett). 
